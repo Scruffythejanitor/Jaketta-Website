04f279072c2dde08925e8003ca5b9c53a8d4a2b1
Andrew <pemberton5150@gmail.com>
2021-03-06 10:20:40 -0700

    testing php file upload

--- forms/contact.php
+++ forms/contact.php
@@ -7,6 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
+  // $receiving_email_address = 'debbie@tjtruckingasphalt.com';
   $receiving_email_address = 'pemberton5150@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
@@ -22,6 +23,7 @@
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
+  $contact->file = $_POST['file'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -36,6 +38,7 @@
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['file'], 'Application');
 
   echo $contact->send();
 ?>
